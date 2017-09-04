ea633aeb2853c2e1d9396c305d631b0b6d3111e1
Daryle Walker <dwalker07@yahoo.com>
2017-09-04 10:54:31 -0400

    Add project for the app.

---
+++ Echo/Echo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Echo
+//
+//  Created by Daryle Walker on 9/4/17.
+//  Copyright © 2017 Daryle Walker. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+}
