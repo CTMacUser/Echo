0904463c825093832947e713d1b4d4ab1a8ed6a0
Daryle Walker <dwalker07@yahoo.com>
2017-09-04 14:44:22 -0400

    Add controls to main window.
    Add fields to read the host, port, and sample data.  Add buttons with
    actions for connecting, disconnecting, and sending data.  Add
    non-editable status fields.
    
    Add connection-status property and mock action code.

--- Echo/Echo/ViewController.swift
+++ Echo/Echo/ViewController.swift
@@ -9,7 +9,22 @@
 import Cocoa
 
 
+// MARK: - Primary Definition
+
+/// Control Echo Transactions.
 class ViewController: NSViewController {
+
+    // MARK: Properties
+
+    /// Whether or not a connection is active.
+    dynamic var connected: Bool = false
+
+    // Outlets
+    @IBOutlet weak var serverAddressField: NSTextField!
+    @IBOutlet weak var portField: NSTextField!
+    @IBOutlet weak var submissionField: NSTextField!
+
+    // MARK: Overrides
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +36,30 @@ class ViewController: NSViewController {
         didSet {
         // Update the view, if already loaded.
         }
+    }
+
+}
+
+// MARK: Actions
+
+extension ViewController {
+
+    @IBAction func connect(_ sender: NSButton) {
+        let hostString = serverAddressField.stringValue
+        guard !hostString.isEmpty && !connected else { return }
+
+        connected = true
+    }
+
+    @IBAction func disconnect(_ sender: NSButton) {
+        connected = false
+    }
+
+    @IBAction func echo(_ sender: NSButton) {
+        let dataString = submissionField.stringValue
+        guard !dataString.isEmpty && connected else { return }
+
+        print("If this was a completed app, the 'echo' would be starting now, using \"\(dataString)\".")
     }
 
 }
